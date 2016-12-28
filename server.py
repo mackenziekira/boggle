1b113f1bd94d70e3bc4febab6419f29d491b4e53
Mackenzie Dolginow <mackenziekira@gmail.com>
2016-12-27 16:17:38 -0800

    rendered board

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import string
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "hello world"
+def homepage():
+    alphabet = string.letters
+    rows = [[random.choice(alphabet).upper() for letter in xrange(6)] for row in xrange(6)]
+    return render_template('board.html', rows=rows)
+
 
 if __name__ == "__main__":
-    app.run('0.0.0.0')
+    app.debug = True
+    app.run()
